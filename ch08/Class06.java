24bbf78ab830932524f2cd2161549861a2b5eb8c
109b30220 <91442668+109b30220@users.noreply.github.com>
2021-10-18 11:48:38 +0800

    Create Class06.java

---
+++ ch08/Class06.java
@@ -0,0 +1,18 @@
+class CCircle{
+	double pi = 3.14;
+	double radius;
+	
+	void show_periphery() {
+		System.out.println("periphery="+2*pi*radius);
+	}
+}
+public class c06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CCircle cir1 = new CCircle();
+		cir1.radius = 3.0;
+		cir1.show_periphery();
+	}
+
+}
