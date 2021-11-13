1470e8524296779081c734063aae99e2857d03e3
109b30220 <91442668+109b30220@users.noreply.github.com>
2021-11-13 09:54:59 +0800

    Create Class06.java

---
+++ ch09/Class06.java
@@ -0,0 +1,14 @@
+public class Class06 {
+	static class C06{
+		static void add2n(int n){
+			int a = (1+n)*n/2;
+			System.out.println(a);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		C06.add2n(5);
+		C06.add2n(10);
+	}
+
+}
