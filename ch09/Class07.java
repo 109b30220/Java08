846b81072fd129b9717bee77999677e94d715309
109b30220 <91442668+109b30220@users.noreply.github.com>
2021-11-13 09:11:50 +0800

    Create Class07.java

---
+++ ch09/Class07.java
@@ -0,0 +1,16 @@
+public class Class07 {
+	static class C07{
+		int ans;
+		static void power(int x, int n){
+			double ans = Math.pow(x, n);
+			System.out.println(ans);
+	}
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		C07.power(2, 5);
+		C07.power(3, 2);
+	}
+
+}
