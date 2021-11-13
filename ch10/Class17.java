ba7b0a4ebdb398ab0ff1db6f677d6ca6cf5372fb
109b30220 <91442668+109b30220@users.noreply.github.com>
2021-11-13 11:01:12 +0800

    Create Class17.java

---
+++ ch10/Class17.java
@@ -0,0 +1,42 @@
+package java10;
+
+import Class17.Namecard.Phone;
+
+class car{
+	protected String owner;
+	protected String id;
+	final void show() {
+		System.out.println("車主姓名:"+owner);
+		System.out.println("車牌號碼:"+id);
+	}
+	void CRectangle(String own,String s){
+		owner = own;
+		id = s;
+	}
+	void Car(String own,String s){
+		
+	}
+}
+class CColor extends car{
+	private String color;
+	CColor(String own,String s,String c){
+		owner = own;
+		id = s ;
+		color =c ;
+	}
+	void CColor1(){
+		show();
+		System.out.println("車身顏色:"+color);
+	}
+}
+public class Class17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CColor mycar = new CColor("Riaan","A1-2345","Black");
+		mycar.CColor1();
+	}
+
+}
+//子類別show有錯誤。
+//改成這樣(?
