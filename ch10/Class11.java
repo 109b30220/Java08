181f862acf63d448bf817beb85d2291dfe830f09
109b30220 <91442668+109b30220@users.noreply.github.com>
2021-11-13 11:45:58 +0800

    Create Class11.java

---
+++ ch10/Class11.java
@@ -0,0 +1,41 @@
+package java10;
+class Cshaped{
+	public double area(){
+		return 0.0 ;
+	}
+	void largest(){
+		if(ans>bns) {
+			
+		}
+	}
+}
+class CCircled extends Cshaped{
+	public double area( ){
+		int a = 3;
+		double ans;
+		return ans = a * a *3.14;
+	}
+}
+class Csqared extends Cshaped{
+	public double area() {
+		int b = 3;
+		int bns;
+		return bns = b*b;
+	}
+}
+class Ctriangled extends Cshaped {
+	public double area() {
+		int c1=3;
+		int c2=3;
+		int cns;
+		return cns = c1 * c2/2; 
+	}
+}
+public class Class11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
