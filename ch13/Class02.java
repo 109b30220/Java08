71eb41ceb364a4348e96fbea3e0dc0732781b63d
109b30220 <91442668+109b30220@users.noreply.github.com>
2021-12-06 11:01:25 +0800

    Create Class02.java

---
+++ ch13/Class02.java
@@ -0,0 +1,19 @@
+package myJava13;
+
+public class Class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=12,den = 0;
+		int ans ;
+		if(den == 0)
+			System.out.println("除數為0");
+		else {
+				ans = num/den;
+				System.out.print("ans="+ans);
+			}
+		
+		System.out.println("end of main() method!!");
+	}
+
+}
